20e5575c123339fd9e5262b04cf0501a0875e2b0
Ryan Mendoza <ryancmendoza@gmail.com>
2018-01-02 22:44:29 -0600

    First stuff

---
+++ PicoBrewPoc/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace PicoBrewPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loginAddress = "https://picobrew.com/account/loginAjax.cshtml";
+            var loginData = new NameValueCollection
+            {
+              { "username", "" },
+              { "password", "" }
+             };
+
+            var client = new CookieAwareWebClient();
+            client.Login(loginAddress, loginData);
+
+            string getAllSessionsForUser = "https://picobrew.com/JSONAPI/Pico/PicoSession.cshtml";
+            string myParameters = "option=getAllSessionsForUser";
+
+            client.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+            string json = client.UploadString(getAllSessionsForUser, myParameters);
+
+            var picoSessions = JsonConvert.DeserializeObject<List<GetAllSessionsForUserResponse>>(json);
+
+            Console.ReadLine();
+        }
+    }
+}
